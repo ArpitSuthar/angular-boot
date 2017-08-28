a69bc7d3b60f5558bf6b5c2ce89e1621f2d7cd6c
Arpit Suthar <nikhilsuthar@gmail.com>
2017-08-28 19:47:49 +0530

    first commit

---
+++ src/main/java/in/arpit/spring/angularboot/domain/Greeting.java
@@ -0,0 +1,22 @@
+package in.arpit.spring.angularboot.domain;
+
+public class Greeting {
+    String id;
+    String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/test/java/in/arpit/spring/angularboot/AngularBootApplicationTests.java
@@ -0,0 +1,16 @@
+package in.arpit.spring.angularboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AngularBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
